d080824281641d716daa3abbbe94325395dfb82c
andrabluum <you@example.com>
2022-04-04 14:02:20 +0300

    "Parandused"

--- src/Rakendus.java
+++ src/Rakendus.java
@@ -1,14 +1,31 @@
-import org.joda.time.*;
-
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Scanner;
 
 public class Rakendus {
     public static void main(String[] args) {
         List<Saal> saalid = new ArrayList<>();
         while(true) {
+            Scanner in = new Scanner(System.in);
+            Integer kasutajaSisestus = in.nextInt();
+            System.out.println("Tere tulemast!");
+            System.out.println("Sisestage nr 1, kui Te soovite pileteid osta.");
+            System.out.println("Sisestage nr 2, kui Te soovite saale hallata.");
+            System.out.println("Sisestage nr 3, kui Te soovite kinokava hallata.");
+            System.out.println("Sisestage nr 0, kui Te ei soovi kinopileteid osta.");
+            if (kasutajaSisestus == 1){
+                
+
+            }
+            else if (kasutajaSisestus == 2){
+
+            }
+            else if (kasutajaSisestus == 3){
+
+            }
+            else if (kasutajaSisestus == 0){
+                break;
+            }
             //kas soovid piletit myya
             //kas soovid saale hallata
             //kas soovid kinokava hallata
